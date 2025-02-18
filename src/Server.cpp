cf7725a830de1915d31c1b3c60cd0102f37daa4e
syan <113340927+Andre1YAN@users.noreply.github.com>
2025-02-18 16:29:39 +0000

    headers

--- src/Server.cpp
+++ src/Server.cpp
@@ -21,6 +21,35 @@ Server &Server::operator=(Server const &src){
 	return *this;
 }
 
+//---------------//Getters
+int Server::GetPort(){return this->port;}
+int Server::GetFd(){return this->server_fdsocket;}
+Client *Server::GetClient(int fd){
+	for (size_t i = 0; i < this->clients.size(); i++){
+		if (this->clients[i].GetFd() == fd)
+			return &this->clients[i];
+	}
+	return NULL;
+}
+Client *Server::GetClientNick(std::string nickname){
+	for (size_t i = 0; i < this->clients.size(); i++){
+		if (this->clients[i].GetNickName() == nickname)
+			return &this->clients[i];
+	}
+	return NULL;
+}
+
+Channel *Server::GetChannel(std::string name)
+{
+	for (size_t i = 0; i < this->channels.size(); i++){
+		if (this->channels[i].GetName() == name)
+			return &channels[i];
+	}
+	return NULL;
+}
+//---------------//Getters
+
+//---------------//Close and Signal Methods
 bool Server::Signal = false;
 void Server::SignalHandler(int signum)
 {
@@ -29,6 +58,19 @@ void Server::SignalHandler(int signum)
 	Server::Signal = true;
 }
 
+void	Server::close_fds(){
+	for(size_t i = 0; i < clients.size(); i++){
+		std::cout << RED << "Client <" << clients[i].GetFd() << "> Disconnected" << WHI << std::endl;
+		close(clients[i].GetFd());
+	}
+	if (server_fdsocket != -1){	
+		std::cout << RED << "Server <" << server_fdsocket << "> Disconnected" << WHI << std::endl;
+		close(server_fdsocket);
+	}
+}
+//---------------//Close and Signal Methods
+
+//---------------//Server Methods
 void Server::init(int port, std::string pass)
 {
 	this->password = pass;
@@ -55,4 +97,96 @@ void Server::init(int port, std::string pass)
 	close_fds();
 }
 
+void Server::set_sever_socket()
+{
+	int en = 1;
+	add.sin_family = AF_INET;
+	add.sin_addr.s_addr = INADDR_ANY;
+	add.sin_port = htons(port);
+	server_fdsocket = socket(AF_INET, SOCK_STREAM, 0);
+	if(server_fdsocket == -1)
+		throw(std::runtime_error("faild to create socket"));
+	if(setsockopt(server_fdsocket, SOL_SOCKET, SO_REUSEADDR, &en, sizeof(en)) == -1)
+		throw(std::runtime_error("faild to set option (SO_REUSEADDR) on socket"));
+	 if (fcntl(server_fdsocket, F_SETFL, O_NONBLOCK) == -1)
+		throw(std::runtime_error("faild to set option (O_NONBLOCK) on socket"));
+	if (bind(server_fdsocket, (struct sockaddr *)&add, sizeof(add)) == -1)
+		throw(std::runtime_error("faild to bind socket"));
+	if (listen(server_fdsocket, SOMAXCONN) == -1)
+		throw(std::runtime_error("listen() faild"));
+	new_cli.fd = server_fdsocket;
+	new_cli.events = POLLIN;
+	new_cli.revents = 0;
+	fds.push_back(new_cli);
+}
 
+void Server::accept_new_client()
+{
+	Client cli;
+	memset(&cliadd, 0, sizeof(cliadd));
+	socklen_t len = sizeof(cliadd);
+	int incofd = accept(server_fdsocket, (sockaddr *)&(cliadd), &len);
+	if (incofd == -1)
+		{std::cout << "accept() failed" << std::endl; return;}
+	if (fcntl(incofd, F_SETFL, O_NONBLOCK) == -1)
+		{std::cout << "fcntl() failed" << std::endl; return;}
+	new_cli.fd = incofd;
+	new_cli.events = POLLIN;
+	new_cli.revents = 0;
+	cli.SetFd(incofd);
+	cli.setIpAdd(inet_ntoa((cliadd.sin_addr)));
+	clients.push_back(cli);
+	fds.push_back(new_cli);
+	std::cout << GRE << "Client <" << incofd << "> Connected" << WHI << std::endl;
+}
+
+void Server::reciveNewData(int fd)
+{
+	std::vector<std::string> cmd;
+	char buff[1024];
+	memset(buff, 0, sizeof(buff));
+	Client *cli = GetClient(fd);
+	ssize_t bytes = recv(fd, buff, sizeof(buff) - 1 , 0);
+	if(bytes <= 0)
+	{
+		std::cout << RED << "Client <" << fd << "> Disconnected" << WHI << std::endl;
+		RmChannels(fd);
+		RemoveClient(fd);
+		RemoveFds(fd);
+		close(fd);
+	}
+	else
+	{ 
+		cli->setBuffer(buff);
+		if(cli->getBuffer().find_first_of("\r\n") == std::string::npos)
+			return;
+		cmd = split_recivedBuffer(cli->getBuffer());
+		for(size_t i = 0; i < cmd.size(); i++)
+			this->parse_exec_cmd(cmd[i], fd);
+		if(GetClient(fd))
+			GetClient(fd)->clearBuffer();
+	}
+}
+//---------------//Server Methods
+
+//---------------//Parsing Methods
+std::vector<std::string> Server::split_recivedBuffer(std::string str)
+{
+	std::vector<std::string> vec;
+	std::istringstream stm(str);
+	std::string line;
+	while(std::getline(stm, line))
+	{
+		size_t pos = line.find_first_of("\r\n");
+		if(pos != std::string::npos)
+			line = line.substr(0, pos);
+		vec.push_back(line);
+	}
+	return vec;
+}
+
+void Server::parse_exec_cmd(std::string &cmd, int fd)
+{
+	//to be done
+}
+//---------------//Parsing Methods
